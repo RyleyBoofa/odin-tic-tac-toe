c08fb41fb4bcd2ecad5003ccc654e4c5e071e7c0
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-09-03 15:56:51 +1000

    feat(gameboard): implement basic gameboard
    User is able to view the gameboard & update each cell via the console.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,29 @@
-console.log("Hello, Tic Tac Toe!");
+// The Odin Project: Project Tic Tac Toe
+
+const gameBoard = (() => {
+    const board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ];
+
+    const logBoard = () => {
+        board.forEach((row) => {
+            console.log(row);
+        });
+    };
+
+    const updateCell = (row, col, value) => {
+        if (board[row][col] === null) {
+            board[row][col] = value;
+        } else {
+            console.warn("This cell is already occupied. Please select an empty cell.");
+        }
+        logBoard();
+    };
+
+    return {
+        logBoard,
+        updateCell,
+    };
+})();
