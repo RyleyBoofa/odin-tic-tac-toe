25446b3701a1626c6bccfb55769e8994e50761d6
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-09-04 17:37:37 +1000

    feat(game): add win conditions
    Created functions in gameboard module to check for a winner.
    Also separated player factory and gameboard module from game module
    for clarity and ease of testing.
    Core game functionality seems to be working - further testing required.

--- src/script.js
+++ src/script.js
@@ -1,68 +1,129 @@
 // The Odin Project: Project Tic Tac Toe
 
-const game = (() => {
-    const createPlayer = (marker) => {
-        const getMarker = () => marker;
+const gameBoard = (() => {
+    const _board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ];
 
-        const takeTurn = (row, col) => {
-            return gameBoard.updateCell(row, col, marker);
-        };
-
-        return {
-            getMarker,
-            takeTurn,
-        };
+    const _logBoard = () => {
+        _board.forEach((row) => {
+            console.log(row);
+        });
     };
 
-    const players = [createPlayer("X"), createPlayer("O")];
-    let activePlayer = 0;
-
-    const gameBoard = (() => {
-        const board = [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null],
-        ];
-
-        const logBoard = () => {
-            board.forEach((row) => {
-                console.log(row);
-            });
-        };
-
-        const updateCell = (row, col, value) => {
-            let success;
-
-            if (board[row][col] === null) {
-                board[row][col] = value;
-                success = true;
-            } else {
-                console.warn("This cell is already occupied. Please select an empty cell.");
-                success = false;
+    const _checkRows = () => {
+        for (let row of _board) {
+            const value = row[0];
+            if (value === null) {
+                return null;
+            } else if (row.every((cell) => cell === value)) {
+                return value;
             }
+        }
+        return null;
+    };
 
-            logBoard();
+    const _checkCols = () => {
+        for (let col = 0; col < 3; col++) {
+            const value = _board[0][col];
+            let match = true;
+            for (let row of _board) {
+                if (row[col] !== value) {
+                    match = false;
+                }
+            }
+            if (match) return value;
+        }
+        return null;
+    };
 
-            return success;
-        };
+    const _checkDiags = () => {
+        // top-left to bottom-right
+        const topLeft = _board[0][0];
+        let match = true;
+        for (let i = 0; i <= 2; i++) {
+            if (_board[i][i] !== topLeft) {
+                match = false;
+            }
+        }
+        if (match) return topLeft;
 
-        const isBoardFull = () => {
-            return board.every((row) => row.every((cell) => cell !== null));
-        };
+        // bottom-left to top-right
+        const bottomLeft = _board[2][0];
+        match = true;
+        for (let i = 0; i <= 2; i++) {
+            if (_board[2 - i][0 + i] !== bottomLeft) {
+                match = false;
+            }
+        }
+        if (match) return bottomLeft;
 
-        return {
-            logBoard,
-            updateCell,
-            isBoardFull,
-        };
-    })();
+        return null;
+    };
 
-    const playRoundRecursive = () => {
+    const updateCell = (row, col, value) => {
+        let success;
+
+        if (_board[row][col] === null) {
+            _board[row][col] = value;
+            success = true;
+        } else {
+            console.warn("This cell is already occupied. Please select an empty cell.");
+            success = false;
+        }
+
+        _logBoard();
+
+        return success;
+    };
+
+    const isBoardFull = () => {
+        return _board.every((row) => row.every((cell) => cell !== null));
+    };
+
+    const checkForWinner = () => {
+        return _checkRows() || _checkCols() || _checkDiags() || null;
+    };
+
+    return {
+        updateCell,
+        isBoardFull,
+        checkForWinner,
+    };
+})();
+
+function createPlayer(marker) {
+    const getMarker = () => marker;
+
+    const takeTurn = (row, col) => {
+        return gameBoard.updateCell(row, col, marker);
+    };
+
+    return {
+        getMarker,
+        takeTurn,
+    };
+}
+
+const game = (() => {
+    const _players = [createPlayer("X"), createPlayer("O")];
+    let _activePlayer = 0;
+
+    const _playRoundRecursive = () => {
         if (gameBoard.isBoardFull()) {
+            console.log("It's a tie");
             return;
         }
 
-        const player = players[activePlayer];
+        const winner = gameBoard.checkForWinner();
+        if (winner !== null) {
+            console.log(`Winner: ${winner}`);
+            return;
+        }
+
+        const player = _players[_activePlayer];
         const playerMarker = player.getMarker();
         console.log(`${playerMarker}'s turn`);
 
@@ -70,14 +131,14 @@ const game = (() => {
         const col = prompt(`${playerMarker} Enter a column number [0-2]`, 0);
 
         if (player.takeTurn(row, col)) {
-            activePlayer = 1 - activePlayer; // toggle between 0-1
+            _activePlayer = 1 - _activePlayer; // toggle between 0-1
         }
 
-        playRoundRecursive();
+        _playRoundRecursive();
     };
 
     const play = () => {
-        playRoundRecursive();
+        _playRoundRecursive();
         console.log("Gameover");
     };
 
