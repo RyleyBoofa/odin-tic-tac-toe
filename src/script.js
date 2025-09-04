c0e8bb85e9ab325e7d56218f6f569bd5168b4d69
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-09-04 12:41:32 +1000

    feat(game): add game module
    Created a game module which is now the only global item and only
    exposes a play() method - the board and players cannot be touched.
    Calling game.play() in the console will recursively play rounds until
    no empty cells remain. No win conditions implemented as of yet.

--- src/script.js
+++ src/script.js
@@ -1,29 +1,87 @@
 // The Odin Project: Project Tic Tac Toe
 
-const gameBoard = (() => {
-    const board = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null],
-    ];
+const game = (() => {
+    const createPlayer = (marker) => {
+        const getMarker = () => marker;
 
-    const logBoard = () => {
-        board.forEach((row) => {
-            console.log(row);
-        });
+        const takeTurn = (row, col) => {
+            return gameBoard.updateCell(row, col, marker);
+        };
+
+        return {
+            getMarker,
+            takeTurn,
+        };
     };
 
-    const updateCell = (row, col, value) => {
-        if (board[row][col] === null) {
-            board[row][col] = value;
-        } else {
-            console.warn("This cell is already occupied. Please select an empty cell.");
+    const players = [createPlayer("X"), createPlayer("O")];
+    let activePlayer = 0;
+
+    const gameBoard = (() => {
+        const board = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
+        ];
+
+        const logBoard = () => {
+            board.forEach((row) => {
+                console.log(row);
+            });
+        };
+
+        const updateCell = (row, col, value) => {
+            let success;
+
+            if (board[row][col] === null) {
+                board[row][col] = value;
+                success = true;
+            } else {
+                console.warn("This cell is already occupied. Please select an empty cell.");
+                success = false;
+            }
+
+            logBoard();
+
+            return success;
+        };
+
+        const isBoardFull = () => {
+            return board.every((row) => row.every((cell) => cell !== null));
+        };
+
+        return {
+            logBoard,
+            updateCell,
+            isBoardFull,
+        };
+    })();
+
+    const playRoundRecursive = () => {
+        if (gameBoard.isBoardFull()) {
+            return;
         }
-        logBoard();
+
+        const player = players[activePlayer];
+        const playerMarker = player.getMarker();
+        console.log(`${playerMarker}'s turn`);
+
+        const row = prompt(`${playerMarker} Enter a row number [0-2]`, 0);
+        const col = prompt(`${playerMarker} Enter a column number [0-2]`, 0);
+
+        if (player.takeTurn(row, col)) {
+            activePlayer = 1 - activePlayer; // toggle between 0-1
+        }
+
+        playRoundRecursive();
+    };
+
+    const play = () => {
+        playRoundRecursive();
+        console.log("Gameover");
     };
 
     return {
-        logBoard,
-        updateCell,
+        play,
     };
 })();
