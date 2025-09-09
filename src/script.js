fed2bc2b8793313ad5734ca3d8b87b36fba9a9d0
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-09-09 16:43:52 +1000

    refactor(script.js): major refactoring
    Fixed win condition bugs - winner is now properly determined every time.
    Updated GUI rendering so that winning cells are highlighted green.
    Split modules into sections, delimited with "//// SECTION NAME ////".
    Added game.reset() method so that reset button works as expected.
    TO DO:
    - Complete _newGame event handler so a brand new game can be started
    - Style form dialog
    - Update existing markup and styling

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 // The Odin Project: Project Tic Tac Toe
 
 const gameBoard = (() => {
+    //// BOARD STATE ////
     const _board = [
         [null, null, null],
         [null, null, null],
@@ -12,120 +13,110 @@ const gameBoard = (() => {
         cells: null,
     };
 
-    function _logBoard() {
-        _board.forEach((row) => {
-            console.log(row);
-        });
-    }
-
+    //// WIN CHECKS ////
     function _checkRows() {
         for (let row of _board) {
             const value = row[0];
-            if (value === null) {
-                return false;
-            } else if (row.every((cell) => cell === value)) {
-                _result.winner = value;
-                _result.cells = `row:${_board.indexOf(row)}`;
-                return true;
+
+            if (value !== null) {
+                if (row.every((cell) => cell === value)) {
+                    const rowIndex = _board.indexOf(row);
+                    _result.winner = value;
+                    _result.cells = [`${rowIndex}/0`, `${rowIndex}/1`, `${rowIndex}/2`];
+                    return true;
+                }
             }
         }
-        return false;
     }
 
     function _checkCols() {
         for (let col = 0; col < 3; col++) {
             const value = _board[0][col];
-            if (value === null) {
-                return false;
-            } else {
+
+            if (value !== null) {
                 let match = true;
+
                 for (let row of _board) {
                     if (row[col] !== value) {
                         match = false;
                     }
                 }
+
                 if (match) {
                     _result.winner = value;
-                    _result.cells = `col:${col}`;
+                    _result.cells = [`0/${col}`, `1/${col}`, `2/${col}`];
                     return true;
                 }
             }
         }
-        return false;
     }
 
-    function _checkDiags() {
-        // top-left to bottom-right
+    function _checkDiagTopLeft() {
         const topLeft = _board[0][0];
         if (topLeft === null) {
             return false;
-        } else {
-            let match = true;
-            for (let i = 0; i <= 2; i++) {
-                if (_board[i][i] !== topLeft) {
-                    match = false;
-                }
-            }
-            if (match) {
-                _result.winner = topLeft;
-                _result.cells = "diag:TL";
-                return true;
+        }
+
+        let match = true;
+
+        for (let i = 0; i <= 2; i++) {
+            if (_board[i][i] !== topLeft) {
+                match = false;
             }
         }
 
-        // bottom-left to top-right
+        if (match) {
+            _result.winner = topLeft;
+            _result.cells = ["0/0", "1/1", "2/2"];
+            return true;
+        }
+    }
+
+    function _checkDiagBottomLeft() {
         const bottomLeft = _board[2][0];
         if (bottomLeft === null) {
             return false;
-        } else {
-            match = true;
-            for (let i = 0; i <= 2; i++) {
-                if (_board[2 - i][0 + i] !== bottomLeft) {
-                    match = false;
-                }
-            }
-            if (match) {
-                _result.winner = bottomLeft;
-                _result.cells = "diag:BL";
-                return true;
+        }
+
+        let match = true;
+
+        for (let i = 0; i <= 2; i++) {
+            if (_board[2 - i][0 + i] !== bottomLeft) {
+                match = false;
             }
         }
 
-        return false;
+        if (match) {
+            _result.winner = bottomLeft;
+            _result.cells = ["2/0", "1/1", "0/2"];
+            return true;
+        }
     }
 
+    //// BOARD INTERACTION ////
     function updateCell(row, col, value) {
-        let success;
+        let success = false;
 
         if (_board[row][col] === null) {
             _board[row][col] = value;
             success = true;
-        } else {
-            console.warn("This cell is already occupied. Please select an empty cell.");
-            success = false;
         }
-
-        _logBoard();
 
         return success;
     }
 
     function isBoardFull() {
         if (_board.every((row) => row.every((cell) => cell !== null))) {
-            _result = {
-                winner: "tie",
-            };
+            _result.winner = "tie";
             return true;
         }
         return false;
     }
 
     function checkForWinner() {
-        return _checkRows() || _checkCols() || _checkDiags() || false;
-    }
-
-    function getBoard() {
-        return _board;
+        return (
+            _checkRows() || _checkCols() || _checkDiagTopLeft() || _checkDiagBottomLeft() || false
+        );
     }
 
     function resetBoard() {
@@ -134,31 +125,37 @@ const gameBoard = (() => {
                 row[cellIndex] = null;
             });
         });
-        _logBoard();
+    }
+
+    //// GETTERS ////
+    function getBoard() {
+        return _board;
     }
 
     function getResult() {
         return _result;
     }
 
+    //// PUBLIC ////
     return {
         updateCell,
         isBoardFull,
         checkForWinner,
-        getBoard,
         resetBoard,
+        getBoard,
         getResult,
     };
 })();
 
 const game = (() => {
-    let _players;
-    let _activePlayer = 0;
-
+    //// GAME STATE ////
+    let _running = true;
     let _roundsPlayed = 0;
     const MIN_ROUNDS_TO_WIN = 5;
 
-    let _running = true;
+    //// PLAYERS ////
+    let _players;
+    let _activePlayer = 0;
 
     function _createPlayer(name, marker) {
         function getName() {
@@ -180,10 +177,22 @@ const game = (() => {
         };
     }
 
+    //// SETUP ////
     function init(p1, p2) {
         _players = [_createPlayer(p1, "X"), _createPlayer(p2, "O")];
     }
 
+    function reset(winner) {
+        _running = true;
+        _roundsPlayed = 0;
+        _players.forEach((player) => {
+            if (player.getMarker() === winner) {
+                _activePlayer = _players.indexOf(player);
+            }
+        });
+    }
+
+    //// GAMEPLAY ////
     function playRound(row, col) {
         if (_players[_activePlayer].takeTurn(row, col)) {
             _activePlayer = 1 - _activePlayer; // toggle between 0-1
@@ -198,6 +207,7 @@ const game = (() => {
         }
     }
 
+    //// GETTERS ////
     function getIsRunning() {
         return _running;
     }
@@ -206,8 +216,10 @@ const game = (() => {
         return _players[_activePlayer];
     }
 
+    //// PUBLIC ////
     return {
         init,
+        reset,
         playRound,
         getIsRunning,
         getActivePlayer,
@@ -215,6 +227,7 @@ const game = (() => {
 })();
 
 const displayController = (() => {
+    //// PARSE DOM ////
     const _gameBoard = gameBoard.getBoard();
     const _playBtn = document.querySelector("#play-btn");
     const _confirmBtn = document.querySelector("#confirm-btn");
@@ -223,9 +236,11 @@ const displayController = (() => {
     const _buttonsContainer = document.querySelector(".buttons-container");
     const _pageHeading = document.querySelector(".page-heading");
 
+    //// BIND EVENTS ////
     _playBtn.addEventListener("click", _showFormDialog);
     _confirmBtn.addEventListener("click", _initGame);
 
+    //// EVENT HANDLERS ////
     function _showFormDialog() {
         _formDialog.showModal();
     }
@@ -248,26 +263,41 @@ const displayController = (() => {
         if (!game.getIsRunning()) {
             return;
         }
+
         const target = event.target;
         const [row, col] = target.getAttribute("data-cell").split("/");
         const result = game.playRound(row, col);
-        _renderBoard(result);
+
+        _render(result);
     }
 
     function _resetGame() {
+        game.reset(gameBoard.getResult().winner);
         gameBoard.resetBoard();
+
         const grid = document.querySelector(".gameboard-container");
-        Array.from(grid.children).forEach((child) => {
-            child.remove();
-        });
-        _buildGridCells(grid);
+        grid.remove();
+
+        const newBtn = document.querySelector("#new-game-btn");
+        newBtn.remove();
+
+        _buildGrid();
     }
 
+    function _newGame() {
+        // show form dialog
+        // reset everything
+        // init new game
+    }
+
+    //// BUILD GUI ////
     function _buildGrid() {
         const grid = document.createElement("div");
         grid.classList.add("gameboard-container");
         grid.addEventListener("click", _updateGridCell);
+
         _mainContent.appendChild(grid);
+
         _buildGridCells(grid);
     }
 
@@ -289,22 +319,45 @@ const displayController = (() => {
         resetBtn.id = "reset-btn";
         resetBtn.textContent = "RESET";
         resetBtn.addEventListener("click", _resetGame);
+
         _buttonsContainer.appendChild(resetBtn);
     }
 
-    function _renderBoard(result) {
+    function _buildNewGameButton() {
+        const newBtn = document.createElement("button");
+        newBtn.setAttribute("type", "button");
+        newBtn.classList.add("button");
+        newBtn.id = "new-game-btn";
+        newBtn.textContent = "NEW GAME";
+        newBtn.addEventListener("click", _newGame);
+
+        _buttonsContainer.appendChild(newBtn);
+    }
+
+    //// RENDER GUI ////
+    function _render(result) {
+        _renderGameBoard();
+
         if (result) {
-            console.log(result);
+            _buildNewGameButton();
 
             if (result.winner === "tie") {
                 _pageHeading.textContent = "It's a tie!";
             } else {
                 _pageHeading.textContent = `${result.winner} wins!`;
+                result.cells.forEach((cell) => {
+                    const [row, col] = cell.split("/");
+                    const target = document.querySelector(`[data-cell="${row}/${col}"]`);
+                    target.classList.add("winning-cell");
+                });
             }
-        } else {
-            _pageHeading.textContent = `${game.getActivePlayer().getMarker()}'s turn`;
+            return;
         }
 
+        _pageHeading.textContent = `${game.getActivePlayer().getMarker()}'s turn`;
+    }
+
+    function _renderGameBoard() {
         _gameBoard.forEach((row, rowIndex) => {
             row.forEach((cell, cellIndex) => {
                 if (cell !== null) {
